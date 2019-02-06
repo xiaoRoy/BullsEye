1b5ea2cde1852f99bdc3bd7b5e226e2efb435b9d
Jerry Li <xiaoroyg@163.com>
2019-02-06 23:11:30 +0800

    Learned Outlet.

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,24 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue: Int = 0
+    
+    @IBOutlet weak var slider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        currentValue = lroundf(slider.value)
     }
     
     @IBAction func showAlert() {
+        let message = "The value of the slider is \(currentValue)"
+        
         let alert = UIAlertController(
-            title: "Hello World", message: "This is my first app!",
+            title: "Hello World", message: message,
             preferredStyle: .alert)
         
         let action = UIAlertAction(
-            title: "Awesome", style: .default, handler: nil)
+            title: "OK", style: .default, handler: nil)
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
     }
-
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
+    }
 }
 
