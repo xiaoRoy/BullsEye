3c524bdd54413428ff011fb085ffb0cef77efc75
Jerry Li <xiaoroyg@163.com>
2019-02-07 16:41:13 +0800

    Finished calculate score.

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -12,18 +12,42 @@ class ViewController: UIViewController {
     
     var currentValue: Int = 0
     
+    var targetValue = 0
+    
+    var score = 0
+    
+    var round = 0
+    
     @IBOutlet weak var slider: UISlider!
+    
+    @IBOutlet weak var targetLabel: UILabel!
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet weak var roundLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        currentValue = lroundf(slider.value)
+        startNewRound()
     }
     
     @IBAction func showAlert() {
-        let message = "The value of the slider is \(currentValue)"
+        let currentScore = calculateScore()
+        score += currentScore
+        let title: String
+        if currentScore == 100 {
+            title = "Perfect!"
+        } else if currentScore >= 95 {
+            title = "You almost had it!"
+        } else if currentScore >= 90 {
+            title = "Pretty Good!"
+        } else {
+            title = "Not even close..."
+        }
         
+        let message = "You scored \(currentScore)"
         let alert = UIAlertController(
-            title: "Hello World", message: message,
+            title: title, message: message,
             preferredStyle: .alert)
         
         let action = UIAlertAction(
@@ -32,10 +56,33 @@ class ViewController: UIViewController {
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+        startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
         currentValue = lroundf(slider.value)
+    }
+    
+    func generateRandomTargetValue() -> Int {
+        return Int.random(in: 1...100)
+    }
+    
+    func startNewRound() {
+        targetValue = generateRandomTargetValue()
+        currentValue = 50
+        slider.value = Float(currentValue)
+        round += 1
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = "\(round)"
+    }
+    
+    func calculateScore() -> Int {
+        return 100 - abs(currentValue - targetValue)
     }
 }
 
